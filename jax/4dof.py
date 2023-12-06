46a8890821cd170e94a442d1bf9ef6737d520901
Ishaan <ishaanamahajan@gmail.com>
2023-12-06 14:21:27 -0600

    adding 4dof based off projectlets

--- jax/4dof.py
+++ jax/4dof.py
@@ -1,50 +1,31 @@
-import jax
 import jax.numpy as jnp
+from jax import grad, jit
 from collections import namedtuple
 
 # Define the vehicle's state and parameters
-VehicleState = namedtuple('VehicleState', ['v', 'r', 'phi', 'theta'])
-VehicleParam = namedtuple('VehicleParam', ['m', 'Iz', 'Iy', 'Ix', 'a', 'b', 'c', 'd'])
+VehStates = namedtuple('VehStates', ['x', 'y', 'theta', 'v'])
+VehParam = namedtuple('VehParam', ['step', 'l', 'R', 'gamma', 'tau0', 'omega0', 'c1', 'c0', 'I'])
 
-def compute_lateral_forces(v_state, v_param):
-    
-    Fyf = 0  # Front lateral force
-    Fyr = 0  # Rear lateral force
-    return Fyf, Fyr
+def integrate_any(v_st, v_params, controls):
+    x_update = v_st.x + jnp.cos(v_st.theta) * v_st.v * v_params.step
+    y_update = v_st.y + jnp.sin(v_st.theta) * v_st.v * v_params.step
+    theta_update = v_st.theta + (jnp.tan(v_params.steerMap * controls[1]) / v_params.l) * v_st.v * v_params.step
 
-def vehicle_dynamics(v_state, v_param, delta):
-    """
-    Compute the next state of the vehicle.
-    
-    :param v_state: VehicleState, current state of the vehicle
-    :param v_param: VehicleParam, parameters of the vehicle
-    :param delta: float, steering angle (rad)
-    :return: VehicleState, the next state of the vehicle
-    """
+    rGamma = v_params.R * v_params.gamma
+    f1 = v_params.tau0 * controls[2] - (v_params.tau0 * v_st.v / (v_params.omega0 * rGamma))
+    f0 = v_st.v * v_params.c1 / rGamma + v_params.c0
+    ta = f1 - f0
+    ta = jnp.where(jnp.abs(v_st.v) < 1e-9, jnp.maximum(ta, 0), ta)
 
-    # Compute lateral forces
-    Fyf, Fyr = compute_lateral_forces(v_state, v_param)
+    v_update = v_st.v + ((rGamma / v_params.I) * ta) * v_params.step
 
-    # Simplified vehicle dynamics equations
-    v_dot = (Fyf + Fyr) / v_param.m
-    r_dot = (v_param.a * Fyf - v_param.b * Fyr) / v_param.Iz
-    phi_dot = v_state.r
-    theta_dot = v_state.v / v_param.c  # Assuming c is the wheelbase
+    return VehStates(x_update, y_update, theta_update, v_update)
 
-    # Update state
-    new_v_state = VehicleState(
-        v = v_state.v + v_dot,
-        r = v_state.r + r_dot,
-        phi = v_state.phi + phi_dot,
-        theta = v_state.theta + theta_dot
-    )
+# Example usage
+v_st = VehStates(x=0, y=0, theta=0, v=0)
+v_params = VehParam(step=0.01, l=2.5, R=0.3, gamma=1.0, tau0=100, omega0=100, c1=0.5, c0=0.1, I=1500)
+controls = [0, 0.1, 1.0]  # Example control inputs
 
-    return new_v_state
+# Integrate vehicle states
+new_v_st = integrate_any(v_st, v_params, controls)
 
-
-v_state = VehicleState(v=0, r=0, phi=0, theta=0)
-v_param = VehicleParam(m=1500, Iz=1500, Iy=800, Ix=500, a=1.5, b=1.5, c=2.5, d=2.5)
-delta = 0.1  # Steering angle in radians
-
-
-new_v_state = vehicle_dynamics(v_state, v_param, delta)
